a604e0c30d1d0bffd3b69256a7b9055cbb98bf97
Ana Tinoco <oconit.study@gmail.com>
2019-08-06 22:06:32 -0300

    como funciona o login

--- Aulas/Program.cs
+++ Aulas/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Aula1;
 using System;
+using System.Collections.Generic;
 
 namespace Aulas
 {
@@ -15,8 +16,8 @@ namespace Aulas
             p.Idade = 10; // atribuindo o valor 10; // salvo na memoria head, como referencia/endereço. Quando não coloca a referencia, dá o erro null pointer exception
             p.Nome = "Ana";
             
-            Console.WriteLine("Idade da pessoa é:" + p.Idade);
-            Console.ReadKey();
+            //Console.WriteLine("Idade da pessoa é:" + p.Idade);
+            //Console.ReadKey();
 
             Chefe b = new Chefe();
             b.Idade = 30;
@@ -33,7 +34,53 @@ namespace Aulas
             string senha = Console.ReadLine();
 
             //criar usuario
-            Usuario novoUsuario;
+            Usuario novoUsuario = new Usuario();
+            novoUsuario.Login = login;
+            novoUsuario.Senha = senha;
+
+            
+            // cadastrar usuario
+            ListaDeUsuarios.Add(novoUsuario);
+
+            // exibe informações
+            Console.WriteLine("usuario cadastrado");
+            Console.WriteLine("login: " + novoUsuario.Login);
+            Console.ReadKey();
+            Console.WriteLine("Quantidade de usuários cadastrados: " + ListaDeUsuarios.Count);
+            Console.ReadKey();
+
+            // pedir informações de login
+            Console.WriteLine("Entre com login: ");
+            string LoginParaEntrar = Console.ReadLine();
+
+            // pedir informações de senha
+            Console.WriteLine("Entre com senha: ");
+            string SenhaParaEntrar = Console.ReadLine();
+
+            // descobrir se esta cadastrado
+
+            foreach ( Usuario usuario in ListaDeUsuarios)
+            {
+                if(usuario.Login == LoginParaEntrar)
+                {
+                    if(usuario.Senha == SenhaParaEntrar)
+                    {
+                        // redirecionar para uma pagina dentro do sistema
+                        Console.WriteLine("Entra ae");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Login ou senha inválidos");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Login ou senha inválidos");
+                }
+            }
+            Console.ReadKey();
         }
+        static List<Usuario> ListaDeUsuarios = new List<Usuario>();
+        
     }
 }
--- Aulas/Usuario.cs
+++ Aulas/Usuario.cs
@@ -6,6 +6,7 @@ namespace Aula1
 {
     class Usuario
     {
-        public string Nome { get; set; }
+        public string Login { get; set; }
+        public string Senha { get; internal set; }
     }
 }
